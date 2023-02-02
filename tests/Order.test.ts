95b23c0385daa9a39db7a7a629eae3e8ded09512
joepdonkers <joepie.donkers@gmail.com>
2023-02-02 17:15:59 +0100

    added student order test

--- tests/Order.test.ts
+++ tests/Order.test.ts
@@ -12,3 +12,13 @@ test("should detect premium ticket", () => {
 
 	expect(order.calculatePrice()).toBe(13);
 });
+
+test("should detect student order", () => {
+	let movie = new Movie("The Matrix");
+	let screening = new MovieScreening(movie, new Date(2019, 11, 1, 20, 0), 10);
+	let ticket = new MovieTicket(screening, 1, 2, false);
+	let order = new Order(1, true);
+	order.addSeatReservation(ticket);
+
+	expect(order.calculatePrice()).toBe(10);
+});
