bdcf9a86be0928f6afd0c7263de1f52453b32255
Perunaz <caelan.van.eijnsbergen@gmail.com>
2023-02-02 16:23:32 +0100

    added jest

--- tests/Order.test.ts
+++ tests/Order.test.ts
@@ -3,22 +3,12 @@ import { MovieScreening } from "../src/MovieScreening";
 import { MovieTicket } from "../src/MovieTicket";
 import { Order } from "../src/Order";
 
-const chai = require('chai')
-const expect = chai.expect
+test('should detect premium ticket', () => {
+    let movie = new Movie("The Matrix");
+    let screening = new MovieScreening(movie, new Date(2019, 11, 1, 20, 0), 10);
+    let ticket = new MovieTicket(screening, 1, 2, true);
+    let order = new Order(1, true);
+    order.addSeatReservation(ticket);
 
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
-describe('Order Tests', function() {
-    describe('Calculate price tests', function() {
-        it('should detect premium ticket', async function() {
-            let movie = new Movie("The Matrix");
-            let screening = new MovieScreening(movie, new Date(2019, 11, 1, 20, 0), 10);
-            let ticket = new MovieTicket(screening, 1, 2, true);
-            let order = new Order(1, true);
-            order.addSeatReservation(ticket);
-
-            expect(order.calculatePrice()).to.be.equal(13);
-        })
-    })
-})
+    expect(order.calculatePrice()).toBe(13);
+  });
